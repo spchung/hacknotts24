7c746758357d237284ae1a6fd22d680953d8cd79
qinkunzhong <xiaofang1058@gmail.com>
2024-10-26 14:05:53 +0100

    Modify Reason: 1st implementation of graph Author: Aidan 20702503

---
+++ GraphUtils.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from typing import List
+
+
+class Graph:
+    def __init__(self):
+        self.topic_doc_dict = defaultdict(defaultdict)
+        self.doc_topic_dict = defaultdict(defaultdict)
+
+    def add_doc_2_graph(self, doc_name: str, topic_freq: List[tuple]):
+        for topic, freq in topic_freq:
+            self.topic_doc_dict[topic][doc_name] = freq
+            self.doc_topic_dict[doc_name][topic] = freq
+
+    def find_topic_by_doc(self, doc_name: str):
+        return sorted(self.doc_topic_dict[doc_name].items(), key=lambda x: x[1], reverse=True)
+
+    def find_docs_by_topic(self, topic_name: str):
+        return sorted(self.topic_doc_dict[topic_name].items(), key=lambda x: x[1], reverse=True)
+
+    @staticmethod
+    def test_add_doc_2_graph():
+        g = Graph()
+        g.add_doc_2_graph("test", [('NLP', 104), ('SVM', 3), ('idk what to fill', 100)])
+        g.add_doc_2_graph('file2', [('NLP', 1), ('idk what to fill', 1), ('ABCD', 134)])
+        g.add_doc_2_graph('file3', [('NLP', 1), ('idk what to fill', 1), ('ABCD', 130),
+                                    ('brain cell killing while writing test case', 12)])
+        g.add_doc_2_graph('filehaha', [('idk what to fill', 1), ('ABCD', 130), ('haha', 30)])
+        print(g.find_docs_by_topic('NLP'))
+        print(g.find_docs_by_topic('ABCD'))
+        print(g.find_topic_by_doc('test'))
+
+
+Graph.test_add_doc_2_graph()
