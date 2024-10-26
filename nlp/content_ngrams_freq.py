f52c81c70d3718308bba113f2ff4b64bf13cbef6
Melvin Kisam <melvin.sulayman@gmail.com>
2024-10-26 21:47:29 +0100

    changed nlp_output file

--- nlp/content_ngrams_freq.py
+++ nlp/content_ngrams_freq.py
@@ -5,11 +5,13 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
 from pprint import pprint
+from DatabaseUtils import DatabaseUtils
 
 # nltk.download("stopwords")
 
-dir_path = os.path.join("nlp", "CS MSc 10a224ca354c80df988cc2782f81a45a", "Human-AI 111224ca354c80208ccdf1d4e30914ac")
+dir_path = os.path.join("nlp", "Human-AI")
 file_content = ""
+nlp_input = DatabaseUtils.get_raw_text_from_db()
 
 def get_content(file_path):
     content = ""
@@ -26,7 +28,7 @@ for file in os.listdir(dir_path):
         file_content += get_content(file_path)
 
 file_content_strip = strip_markdown.strip_markdown(file_content).lower()
-file_tokens = word_tokenize(file_content_strip)
+file_tokens = word_tokenize(nlp_input.lower())
 
 file_token_sw = []
 for word in file_tokens:
@@ -44,29 +46,29 @@ print("Unigram Frequencies:", str(unigram_freq.most_common(5)))
 print("Bigram Frequencies:", str(bigram_freq.most_common(5)))
 print("Trigram Frequencies:", str(trigram_freq.most_common(5)))
 
-bigram_terms = []
-for bigram, count in bigram_freq.items():
-    if count > 1:
-        term = bigram[0] + " " + bigram[1]
-        bigram_terms.append(term)
+# bigram_terms = []
+# for bigram, count in bigram_freq.items():
+#     if count > 1:
+#         term = bigram[0] + " " + bigram[1]
+#         bigram_terms.append(term)
 
-result = []
-for file in os.listdir(dir_path):
-    if file.endswith(".md"):
-        file_path = os.path.join(dir_path, file)
-        current_file = get_content(file_path)
-        current_file_strip = strip_markdown.strip_markdown(current_file).lower()
-        file_summary = {
-            'file': file,
-            }
+# result = []
+# for file in os.listdir(dir_path):
+#     if file.endswith(".md"):
+#         file_path = os.path.join(dir_path, file)
+#         current_file = get_content(file_path)
+#         current_file_strip = strip_markdown.strip_markdown(current_file).lower()
+#         file_summary = {
+#             'file': file,
+#             }
           
-        current_row = []
-        for term in bigram_terms:
-            count = current_file_strip.count(term)
-            current_row.append((term, count))
-        file_summary["row"] = current_row
-        result.append(file_summary)
+#         current_row = []
+#         for term in bigram_terms:
+#             count = current_file_strip.count(term)
+#             current_row.append((term, count))
+#         file_summary["row"] = current_row
+#         result.append(file_summary)
 
 
-pprint(result)
-pprint(len(result))
+# pprint(result)
+# pprint(len(result))
